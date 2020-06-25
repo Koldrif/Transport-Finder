44baed62a607e95a8b0f53aab0e05b5742e62fe1
Ariken12 <ariken12.nam@gmail.com>
2020-06-25 14:22:53 +0300

    add init class

--- DataBase/ClientsType.py
+++ DataBase/ClientsType.py
@@ -4,3 +4,16 @@ DB_SERVER = 'localhost'
 LOGIN = 'server'
 PASSWORD = 'secret'
 DATABASE = 'DataBase'
+CHARSET = 'utf8'
+
+
+class ClientsType:
+    def __init__(self):
+        self.connect = pms.connect(
+            host=DB_SERVER,
+            user=LOGIN,
+            password=PASSWORD,
+            db=DATABASE,
+            charset=CHARSET
+        )
+
