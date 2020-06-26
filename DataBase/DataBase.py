044772b6c10a18795ea7fb6b27e294644d4e6309
Ariken12 <ariken12.nam@gmail.com>
2020-06-26 14:23:22 +0300

    add method for insert owner

--- DataBase/DataBase.py
+++ DataBase/DataBase.py
@@ -39,3 +39,17 @@ class DataBase:
                                           Serial=pass_ser,
                                           Ownership=ownership,
                                           Brand=brand))
+
+    def insert_owner(self, inn, title, registred_at, license_number, reg_address, implement_address, risk_category):
+        request = "INSERT INTO transportfinder.owners (INN, Title, Registred_at, License_number, Reg_adress, " \
+                  "Implement_adress, Risk_category) VALUES ('{INN}', '{Title}', '{Registred_at}', '{License_number}', " \
+                  "'{Reg_address}', '{Implement_address}', 'Risk_category'); "
+        with self.connect:
+            cursor = self.connect.cursor()
+            cursor.execute(request.format(INN=inn,
+                                          Title=title,
+                                          Registred_at=registred_at,
+                                          License_number=license_number,
+                                          Reg_address=reg_address,
+                                          Implement_address=implement_address,
+                                          Risk_category=risk_category))
