162da1d2052d9291ca44d7b99842c66699fcfe37
Ariken12 <ariken12.nam@gmail.com>
2020-06-26 14:23:03 +0300

    add method for insert transport

--- DataBase/DataBase.py
+++ DataBase/DataBase.py
@@ -25,3 +25,17 @@ class DataBase:
             rows = cursor.fetchall()
             return rows
 
+    def insert_transport(self, vin, state_registr_mark, region,
+                         date_of_issue, pass_ser, ownership, brand):
+        request = "INSERT INTO transportfinder.transport (VIN, State_Registr_Mark, Region, Date_of_issue, pass_ser, " \
+                  "Ownership, brand) VALUES ('{VIN}', '{SRM}', '{Region}', '{DateOfIssue}', '{Serial}', " \
+                  "'{Ownership}', '{Brand}') "
+        with self.connect:
+            cursor = self.connect.cursor()
+            cursor.execute(request.format(VIN=vin,
+                                          SRM=state_registr_mark,
+                                          Region=region,
+                                          DateOfIssue=date_of_issue,
+                                          Serial=pass_ser,
+                                          Ownership=ownership,
+                                          Brand=brand))
