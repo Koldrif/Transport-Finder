619802db9d29d935a8958955754db0236760bfff
Koldrif <vorobiov.777@yandex.ru>
2020-07-09 19:31:38 +0300

    Update Запрос базы данных для связующей таблицы transport-owners.sql

--- DataBase/Запрос базы данных для связующей таблицы transport-owners.sql
+++ DataBase/Запрос базы данных для связующей таблицы transport-owners.sql
@@ -13,3 +13,19 @@ FROM
 	join owners on owners.Owner_id = transport_owners.Owner_id
 WHERE
 	owners.INN = '7802519724';
+
+UPDATE
+	`transportfinder`.`transport`
+SET
+	`VIN` = 'ВИН',
+	`State_Registr_Mark` = 'Регистрационный знак',
+	`Region` = 'Регион',
+	`Date_of_issue` = 'Дата выпуска',
+	`pass_ser` = 'Серия паспорта',
+	`Ownership` = 'Тип владения',
+	`brand` = 'Брэнд',
+	`type` = 'Вид',
+	`Registred_at` = 'Зарегестрировано',
+	`License number` = 'Номери лицензии'
+WHERE
+	(`transport_id` = 'ID трансопрта');
