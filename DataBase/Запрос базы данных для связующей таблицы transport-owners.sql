9a9c229a99c154c5cff54b96d1644ae0a9089213
Koldrif <vorobiov.777@yandex.ru>
2020-07-09 19:07:08 +0300

    Добавлены запросы

--- DataBase/Запрос базы данных для связующей таблицы transport-owners.sql
+++ DataBase/Запрос базы данных для связующей таблицы transport-owners.sql
@@ -1,5 +1,15 @@
-SELECT owners.INN as ИНН, owners.Title as Название, transport.VIN as ВИН, `transport`.`brand` as Марка, `transport`.`State_Registr_Mark` as Номер, `transport`.`Region` as Регион, `transport`.`Ownership` as `Тип владения` FROM transport
-	join transport_owners
-		on transport.transport_id = transport_owners.transport_id
-	join owners 
-		on owners.Owner_id = transport_owners.transport_id;
+SELECT
+	owners.INN as ИНН,
+	owners.Title as Название,
+	transport.transport_id as 'ID Тс',
+	transport.VIN as ВИН,
+	`transport`.`brand` as Марка,
+	`transport`.`State_Registr_Mark` as Номер,
+	`transport`.`Region` as Регион,
+	`transport`.`Ownership` as `Тип владения`
+FROM
+	transport
+	join transport_owners on transport.transport_id = transport_owners.transport_id
+	join owners on owners.Owner_id = transport_owners.Owner_id
+WHERE
+	owners.INN = '7802519724';
