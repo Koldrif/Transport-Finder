eb30af50b9e30af309da19617d04c00856a488bf
Koldrif <vorobiov.777@yandex.ru>
2020-07-10 14:21:33 +0300

    Update sql

--- DataBase/Очистить Владельцев.sql
+++ DataBase/Очистить Владельцев.sql
@@ -1,4 +1,52 @@
-SET SQL_SAFE_UPDATES=0;
-DELETE FROM owners;
-ALTER TABLE owners AUTO_INCREMENT=0;
-SET SQL_SAFE_UPDATES=0;
+SET
+    SQL_SAFE_UPDATES = 0;
+
+DELETE FROM
+    owners;
+
+ALTER TABLE
+    owners AUTO_INCREMENT = 0;
+
+SET
+    SQL_SAFE_UPDATES = 0;
+
+INSERT INTO
+    `transportfinder`.`owners` (
+        `INN`,
+        `OGRN`,
+        `Title`,
+        `Registered_at_date`,
+        `License_number`,
+        `Reg_address`,
+        `Implement_address`,
+        `Risk_category`,
+        `Starts_at`,
+        `Duration_hours`,
+        `Last inspec`,
+        `Purpose`,
+        `other_reason`,
+        `form_of_holding`,
+        `Performs_with`,
+        `Punishment`,
+        `Description`
+    )
+VALUES
+    (
+        'INN',
+        'OGRN',
+        'Название',
+        'Registered_at',
+        'License_number',
+        'Reg_address',
+        'Implement_address',
+        'Risk_cat',
+        'Starts_at',
+        'Duration',
+        'Last_Inspect',
+        'Purpose',
+        'Other_reason',
+        'Form_of_holding',
+        'Performs_with',
+        'Punishment',
+        'Description'
+    );
