b404a924b589587c97655e3c288206b353ece9f1
Koldrif <vorobiov.777@yandex.ru>
2020-07-10 14:28:37 +0300

    Добавлен запрос

--- DataBase/DataBase.py
+++ DataBase/DataBase.py
@@ -230,10 +230,58 @@ class DataBase:
                            date_of_issue='Н/Д', pass_ser='Н/Д', ownership='Н/Д',
                            brand='Н/Д', ttype='Н/Д', registred_at='Н/Д', license_number='Н/Д',
                            **trash):
-        request = "INSERT INTO `transportfinder`.`transport` " \
-                  "(`VIN`, `State_Registr_Mark`, `Region`, `Date_of_issue`, `pass_ser`, `Ownership`, `brand`, `type`, `Registred_at`, `License number`) " \
-                  "VALUES " \
-                  "('{VIN}', '{SRM}', '{Region}', '{DateOfIssue}', '{Serial}', '{Ownership}', '{Brand}', '{TType}', '{Registred_at}', '{License_number}');" \
+        request = '''INSERT INTO
+    `transportfinder`.`transport` (
+        `VIN`,
+        `State_Registr_Mark`,
+        `Region`,
+        `Date_of_issue`,
+        `pass_ser`,
+        `Ownership`,
+        `End_date_of_ownership`,
+        `brand`,
+        `model`,
+        `type`,
+        `Registred_at`,
+        `License number`,
+        `Status`,
+        `Action_with_vehicle`,
+        `Categorized`,
+        `Number_of_cat_reg`,
+        `Data_in_cat_reg`,
+        `ATP`,
+        `Model_from_cat_reg`,
+        `Owner_from_cat_reg`,
+        `Purpose_into_cat_reg`,
+        `Category`,
+        `Date_of_cat_reg`
+    )
+VALUES
+    (
+        'VIN',
+        'SRM',
+        'Region',
+        'Date_of_issue',
+        'Pass_ser',
+        'Ownership',
+        'End_date_of_ownership',
+        'Brand',
+        'Model',
+        'Type',
+        'Registred_at',
+        'License_number',
+        'Status',
+        'Action_with_vehicle',
+        'Categorized',
+        'Number_of_cat_reg',
+        'Data_in_cat_reg',
+        'ATP',
+        'Model_from_cat_reg',
+        'Ower_from_cat_reg',
+        'Purpose_into_cat_reg',
+        'Category',
+        'Date_of_cat_reg'
+    );''' \
                   "".format(VIN=vin,
                             SRM=state_registr_mark,
                             Region=region,
--- DataBase/Очистить Транспорт.sql
+++ DataBase/Очистить Транспорт.sql
@@ -1,4 +1,64 @@
-SET SQL_SAFE_UPDATES=0;
-DELETE FROM transport;
-ALTER TABLE transport AUTO_INCREMENT=0;
-SET SQL_SAFE_UPDATES=0;
+SET
+    SQL_SAFE_UPDATES = 0;
+
+DELETE FROM
+    transport;
+
+ALTER TABLE
+    transport AUTO_INCREMENT = 0;
+
+SET
+    SQL_SAFE_UPDATES = 0;
+
+INSERT INTO
+    `transportfinder`.`transport` (
+        `VIN`,
+        `State_Registr_Mark`,
+        `Region`,
+        `Date_of_issue`,
+        `pass_ser`,
+        `Ownership`,
+        `End_date_of_ownership`,
+        `brand`,
+        `model`,
+        `type`,
+        `Registred_at`,
+        `License number`,
+        `Status`,
+        `Action_with_vehicle`,
+        `Categorized`,
+        `Number_of_cat_reg`,
+        `Data_in_cat_reg`,
+        `ATP`,
+        `Model_from_cat_reg`,
+        `Owner_from_cat_reg`,
+        `Purpose_into_cat_reg`,
+        `Category`,
+        `Date_of_cat_reg`
+    )
+VALUES
+    (
+        'VIN',
+        'SRM',
+        'Region',
+        'Date_of_issue',
+        'Pass_ser',
+        'Ownership',
+        'End_date_of_ownership',
+        'Brand',
+        'Model',
+        'Type',
+        'Registred_at',
+        'License_number',
+        'Status',
+        'Action_with_vehicle',
+        'Categorized',
+        'Number_of_cat_reg',
+        'Data_in_cat_reg',
+        'ATP',
+        'Model_from_cat_reg',
+        'Ower_from_cat_reg',
+        'Purpose_into_cat_reg',
+        'Category',
+        'Date_of_cat_reg'
+    );
