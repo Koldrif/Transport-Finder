8213e6c446023fe120869863e7b2a94f64669026
Ariken12 <ariken12@github.com>
2020-07-17 12:41:14 +0300

    update test.py

--- test.py
+++ test.py
@@ -1,6 +1,12 @@
 from DataBase.DataBase import DataBase as Database
 import Functions as funcs
+import os
 
 database = Database(host='localhost', user='root', password='6786')
 
-funcs.old_format('222391520487', database)
+path = './Реестры и т.д/3 - Реестры категорирования (Актуальны на 19.06.2020)/'
+
+for name in os.listdir(path):
+    index = name.split('-')[2]
+    if int(index) > 3:
+        database.read_category_register(path + name)
