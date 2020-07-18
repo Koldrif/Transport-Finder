8dbc1df85bc033ed358305809d4bf84f4f511393
Ariken12 <ariken12@github.com>
2020-07-18 12:25:35 +0300

    clean flask_app

--- flask_app.py
+++ flask_app.py
@@ -1,7 +1,4 @@
 from tokens import VK_API_TOKEN, VK_GROUP_ID
-import vk_api
-from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
-from random import randint
 from VkApi import VkSession
 
 vk_session = VkSession(token=VK_API_TOKEN, vkgroup=VK_GROUP_ID)
