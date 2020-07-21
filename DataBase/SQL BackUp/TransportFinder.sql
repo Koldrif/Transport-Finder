5c68e664c44c59af8db76e84003aeb4ec555f4e3
Koldrif <vorobiov.777@yandex.ru>
2020-07-21 10:40:59 +0300

    Добавлена БД

---
+++ DataBase/SQL BackUp/TransportFinder.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5c4f83d7a564b2ff725200b3eff6afa8ff92b33e4e5cc40683fa40b7411263ec
+size 191472059
--- DataBase/SQL requests/Запрос на поиск лицензий и грз.sql
+++ DataBase/SQL requests/Запрос на поиск лицензий и грз.sql
@@ -1,2 +1,3 @@
-SELECT `License number`, `State_Registr_Mark` FROM transportfinder.transport
-	WHERE `License number` = 'Лицензия' OR `State_Registr_Mark` = 'ГРЗ';
+version https://git-lfs.github.com/spec/v1
+oid sha256:e063a95034f3aee84890a6b5a1a02027f70a9583147736a16b7a51969b498952
+size 158
