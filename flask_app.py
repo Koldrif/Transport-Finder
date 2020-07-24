824b05bd5b5da4112ef6acdf582aa454ac41c949
Koldrif <vorobiov.777@yandex.ru>
2020-07-24 15:52:44 +0300

    Update Functions.py

--- flask_app.py
+++ flask_app.py
@@ -4,7 +4,7 @@ from DataBase.DataBase import DataBase as Database
 
 
 def main():
-    database = None #Database(host='localhost', user='root', password='6786')
+    database = Database(host='localhost', user='root', password='6786')
     vk_session = VkSession(database, token=VK_API_TOKEN, vkgroup=VK_GROUP_ID, users_filename='users.txt', administrators_filename='administrators.txt')
     print('Бот запущен')
     while True:
