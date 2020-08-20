9f6d3dd650d917d60393d80a32be32d3885e9249
Ariken <ariken12.nam@gmail.com>
2020-08-20 21:15:43 +0300

    add comments

--- test.py
+++ test.py
@@ -7,5 +7,11 @@ database = Database(host='127.0.0.1', user='root', password='F0ll0wMy$QL', db='t
 
 print('Время начала:', time.time())
 
+#database.read_license_and_bus('D:\\YandexDisk\\Programming\\Transport-Finder\\Реестры и т.д\\1 - Реестр лицензий и автобус\\3 - БД+ лицензий и автобусов (без ИНН) Краснодар Адыгея 23.07.2020 .xlsx', 'license_3')
+#Краснодар
+
+database.read_license_and_bus('D:\\YandexDisk\\Programming\\Transport-Finder\\Реестры и т.д\\1 - Реестр лицензий и автобус\\2 - БД- лицензий Санкт-Петерубург, ЛО.03.07.2020.xlsx', 'license_2')
 database.read_license_and_bus('D:\\YandexDisk\\Programming\\Transport-Finder\\Реестры и т.д\\1 - Реестр лицензий и автобус\\2 - БД+ автобусов Санкт-Петерубург, ЛО.03.07.2020 Без ИНН.xlsx', 'bus_2')
+#Санкт-Питербург
+
 print('Время конца:', time.time())        
